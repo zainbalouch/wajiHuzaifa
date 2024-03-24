5b2eda5731a5928f8cb5f66ab9967237506251a6
zainbalouch <zainuleman786@gmail.com>
2024-03-24 06:55:28 +0500

    first comment

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::get('/', [HomeController::class, 'homePage']);
+Route::get('/about', [HomeController::class, 'aboutPage']);
+
+
